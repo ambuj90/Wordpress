e56f2dc9c7ede31eadd1d52c57f17b3d50088466
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2021-08-03 14:57:29 +0530

    Create All-portfolio-hidden.js

---
+++ All-portfolio-hidden.js
@@ -0,0 +1,9 @@
+//You can add this under Custom > JavaScript in the Customizer.
+jQuery( document ).ready(function() {
+ jQuery('.all-sort').click();
+});
+
+//If you wanted to hide the “sort portfolio” button you can add this under Custom > CSS in the Customizer.
+[data-filter=".all-sort"] {
+    display:none!important;
+}
